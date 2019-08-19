a1f70f42fb69c1ccb84e42f9f5717a466fb3ae82
Veronica Lopez <nicaa0695@gmail.com>
2019-08-19 18:56:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -46,11 +46,11 @@ class Student
   end
   
   def self.new_from_db(row)
-    new_student = self.new(name, grade, id)
-    new_student.id = row[0]
-    new_student.name = row[1]
-    new_student.grade = row[2]
-    new_student 
+    name = row[1]
+    grade = row[2]
+    id = row[0]
+    student = self.new(name, grade, id)
+    student
   end
 
 
