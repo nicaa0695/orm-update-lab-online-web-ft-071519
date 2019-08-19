8b354a18a5f3b8e774c82285cd283c02356b1b97
Veronica Lopez <nicaa0695@gmail.com>
2019-08-19 18:55:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,13 +40,13 @@ class Student
   end
   
   def self.create(name, grade)
-    student = Student.new(name, grade)
+    student = Sudent.new(name, grade)
     student.save
     student 
   end
   
   def self.new_from_db(row)
-    new_student = self.new 
+    new_student = self.new(name, grade, id)
     new_student.id = row[0]
     new_student.name = row[1]
     new_student.grade = row[2]
